cd1591580c082990dd9c4d23598b42d67bede9fc
Bruno Bonavigo <71879062+Bonavigo@users.noreply.github.com>
2021-03-28 20:46:49 -0300

    Add files via upload

---
+++ app/global.php
@@ -0,0 +1,11 @@
+<?php 
+	session_start();
+	
+	define("HOST", "localhost");
+	define("USER", "root");
+	define("PASS", "");
+	define("DB", "system");
+	$conn = mysqli_connect(HOST, USER, PASS, DB) or die("Não é possível conectar-se ao banco de dados.");
+	$titulo_site = "System UNEP";
+	$imagem_site = "https://www.habbo.com.br/habbo-imaging/badge/b09114s02010s11017s55011t2711481455e142351964ef03b8868295e5ec5.gif";
+?>
